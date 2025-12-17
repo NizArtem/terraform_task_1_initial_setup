4d0ec27ed7ebd13b747220b8c9bb3b526f6d13c2
Artem Nizamiiev <upuyayua@gmail.com>
2025-12-17 14:52:14 +0200

    solved3

--- my-terraform-project/main.tf
+++ my-terraform-project/main.tf
@@ -11,15 +11,15 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group2" "example2" {
+resource "azurerm_resource_group" "example" {
   name     = "example-resources2"
   location = "East Europe"
 }
 
 resource "azurerm_storage_account" "example" {
   name                     = "examplestorageacc2"
-  resource_group_name      = azurerm_resource_group2.example2.name
-  location                 = azurerm_resource_group2.example2.location
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
